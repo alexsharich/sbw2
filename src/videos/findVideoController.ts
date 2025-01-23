50246a8ee808af56a75822404a30c19f4073dc83
alexsharich <alexsharich@gmail.com>
2025-01-23 22:25:41 +0300

    add express validator

--- src/videos/findVideoController.ts
+++ src/videos/findVideoController.ts
@@ -1,12 +1,11 @@
 import {Request, Response} from 'express'
-import {db} from '../db/db'
 import {videosRepository} from "../videos-repository/videosRepository";
 
 export const findVideoController = (req: Request, res: Response<any>) => {
 
-    const foundedVideovideo =videosRepository.findVideo(req.params.id ? req.params.id : '')
+    const foundedVideovideo = videosRepository.findVideo(req.params.id ? req.params.id : '')
 
-    if(foundedVideovideo === false){
+    if (foundedVideovideo === false) {
         res.sendStatus(404)
         return
     }
--- src/videos/updateVideoController.ts
+++ src/videos/updateVideoController.ts
@@ -1,5 +1,4 @@
 import {Request, Response} from 'express'
-import {db} from '../db/db'
 import {videosRepository} from "../videos-repository/videosRepository";
 import {Resolutions} from "../input-output-types/video-types";
 
